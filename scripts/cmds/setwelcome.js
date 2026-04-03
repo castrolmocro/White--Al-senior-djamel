22d5e071138c6dd811b20ae0869e56d3a4aef378
castrolmocro <castrolmocro@gmail.com>
2026-04-03 17:16:50 +0100

    Update setwelcome.js

--- scripts/cmds/setwelcome.js
+++ scripts/cmds/setwelcome.js
@@ -7,7 +7,7 @@ module.exports = {
 		version: "1.7",
 		author: "NTKhang",
 		countDown: 5,
-		role: 1,
+		role: 2,
 		description: {
 			vi: "Chỉnh sửa nội dung tin nhắn chào mừng thành viên mới tham gia vào nhóm chat của bạn",
 			en: "Edit welcome message content when new member join your group chat"
